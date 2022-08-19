c5c2642f2af5b95b505d62749ecbad9e8c091b7b
shahin <shahin@users.noreply.github.com>
2022-08-19 12:47:12 -0700

    Upgrade pre-commit version to fix crash

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,12 @@
 set -euo pipefail
 set -x
 
+version="2.20.0.post1.dev3"
+
 # install pre-commit hooks
 curl \
     --location \
     --remote-name \
-    https://github.com/shahin/pre-commit/releases/download/v2.20.0.post1.dev1/pre-commit-2.20.0.post1.dev1.pyz
-python3 pre-commit-2.20.0.post1.dev1.pyz install
-rm pre-commit-2.20.0.post1.dev1.pyz
+    https://github.com/shahin/pre-commit/releases/download/v${version}/pre-commit-${version}.pyz
+python3 pre-commit-${version}.pyz install
+rm pre-commit-${version}.pyz
